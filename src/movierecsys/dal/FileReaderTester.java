fe2709469f99775b3c59305e28a1fc4edaccbcf8
oliverager <114471849+oliverager@users.noreply.github.com>
2022-11-11 10:20:17 +0100

    Initial commit

---
+++ src/movierecsys/dal/FileReaderTester.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package movierecsys.dal;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Stream;
+import movierecsys.be.Movie;
+
+/**
+ *
+ * @author pgn
+ */
+public class FileReaderTester
+{
+
+    /**
+     * Example method. This is the code I used to create the users.txt files.
+     *
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException
+    {
+        MovieDAO movieDao = new MovieDAO();
+        List<Movie> allMovs = movieDao.getAllMovies();
+        for (Movie allMov : allMovs)
+        {
+            System.out.println(allMov.getTitle());
+        }
+        System.out.println("Movie count: " + allMovs.size());
+    }
+
+   
+}
---
+++ src/movierecsys/dal/MovieDAO.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package movierecsys.dal;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import movierecsys.be.Movie;
+
+/**
+ * @author pgn
+ */
+public class MovieDAO {
+
+    private static final String MOVIE_SOURCE = "data/movie_titles.txt";
+
+    /**
+     * Gets a list of all movies in the persistence storage.
+     *
+     * @return List of movies.
+     * @throws java.io.FileNotFoundException
+     */
+    public List<Movie> getAllMovies() throws FileNotFoundException, IOException {
+        List<Movie> allMovies = new ArrayList<>();
+        File file = new File(MOVIE_SOURCE);
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                try {
+                    Movie mov = stringArrayToMovie(line);
+                    allMovies.add(mov);
+                } catch (Exception ex) {
+                    //Do nothing we simply do not accept malformed lines of data.
+                    //In a perfect world you should at least log the incident.
+                }
+            }
+        }
+        return allMovies;
+    }
+
+    /**
+     * Reads a movie from a , s
+     *
+     * @param t
+     * @return
+     * @throws NumberFormatException
+     */
+    private Movie stringArrayToMovie(String t) {
+        String[] arrMovie = t.split(",");
+
+        int id = Integer.parseInt(arrMovie[0]);
+        int year = Integer.parseInt(arrMovie[1]);
+        String title = arrMovie[2];
+        if (arrMovie.length > 3) {
+            for (int i = 3; i < arrMovie.length; i++) {
+                title += "," + arrMovie[i];
+            }
+        }
+        Movie mov = new Movie(id, year, title);
+        return mov;
+    }
+
+    /**
+     * Creates a movie in the persistence storage.
+     *
+     * @param releaseYear The release year of the movie
+     * @param title       The title of the movie
+     * @return The object representation of the movie added to the persistence
+     * storage.
+     */
+    private Movie createMovie(int releaseYear, String title) {
+        //TODO Create movie.
+        return null;
+    }
+
+    /**
+     * Deletes a movie from the persistence storage.
+     *
+     * @param movie The movie to delete.
+     */
+    private void deleteMovie(Movie movie) {
+        //TODO Delete movie
+    }
+
+    /**
+     * Updates the movie in the persistence storage to reflect the values in the
+     * given Movie object.
+     *
+     * @param movie The updated movie.
+     */
+    private void updateMovie(Movie movie) {
+        //TODO Update movies
+    }
+
+    /**
+     * Gets a the movie with the given ID.
+     *
+     * @param id ID of the movie.
+     * @return A Movie object.
+     */
+    private Movie getMovie(int id) {
+        //TODO Get one Movie
+        return null;
+    }
+
+}
